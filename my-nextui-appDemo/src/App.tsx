d37cd932c8b09ccceb50f9059a3abba2c7f0d357
Irving Penalo <72474402+Perlishnov@users.noreply.github.com>
2025-01-14 00:30:34 -0400

    Se arreglo la ruta para Propietarios

--- my-nextui-appDemo/src/App.tsx
+++ my-nextui-appDemo/src/App.tsx
@@ -7,6 +7,10 @@ import NotFoundPage from "./pages/NotFoundPage";
 import DetallesFarmacia from "./pages/Inspector/DetallesFarmacia"; // Importar el componente
 import InspectorCuenta from "./pages/Inspector/InspectorCuenta"; // Importar el componente
 
+// Propietario
+import PropietarioDashboard from "./pages/Propietario/dashboard";
+import PropietarioMyPharmacies from "./pages/Propietario/MyPharmacies";
+
 // Inspector
 import DashboardInspector from "./pages/Inspector/dashboardIns";
 import EvaluationsList from "./pages/Inspector/EvaluationsList";
@@ -23,6 +27,9 @@ function App() {
       <Route element={<EvaluationsList />} path="/inspector/evaluations" />
       <Route element={<DetallesFarmacia />} path="/inspector/farmacia/:farmaciaId" />
       <Route element={<InspectorCuenta />} path="/inspector/account" /> {/* Nueva ruta */}
+      { /* Propietario */}
+      <Route element={<PropietarioDashboard />} path="/propietario/dashboard" />
+      <Route element={<PropietarioMyPharmacies />} path="/propietario/farmacias" />
 
       {/* Página 404 */}
       <Route element={<NotFoundPage />} path="*" />
